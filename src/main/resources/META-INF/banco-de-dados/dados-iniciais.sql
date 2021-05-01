4bfc7bbd13f5a66203f2a6b82db3d30a687c3a05
Thiago Orlandini <thiagorlandini@hotmail.com>
2021-05-01 10:46:10 -0300

    gitignore

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -1,4 +1,3 @@
-  
 insert into produto (id, nome, preco, data_criacao, descricao) values (1, 'Kindle', 499.0, date_sub(sysdate(), interval 1 day), 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
 insert into produto (id, nome, preco, data_criacao, descricao) values (3, 'Câmera GoPro Hero 7', 1400.0, date_sub(sysdate(), interval 1 day), 'Desempenho 2x melhor.');
 
@@ -11,6 +10,6 @@ insert into pedido (id, cliente_id, data_criacao, total, status) values (2, 1, s
 insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (1, 1, 499, 2);
 insert into item_pedido (pedido_id, produto_id, preco_produto, quantidade) values (2, 1, 499, 1);
 
-insert into pagamento_cartao (pedido_id, status, numero_cartao) values (2, 'PROCESSANDO', '123');
+insert into pagamento (pedido_id, status, tipo_pagamento, numero_cartao, codigo_barras) values (2, 'PROCESSANDO', 'cartao', '123', null);
 
 insert into categoria (id, nome) values (1, 'Eletrônicos');
